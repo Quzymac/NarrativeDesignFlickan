dc0c025af9abc161c0778a7f4c69896a1325b0ed
Timeken <Tim@ekenberg.net>
2018-11-14 15:49:27 +0100

    Climbing and basic interact

---
+++ Assets/Scripts/EnviormentScripts/Interactable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField]
+    private string text;
+    public Text textDisplay;
+    public void Start()
+    {
+
+    }
+
+    public void PlayText()
+    {
+        textDisplay.text = text;
+    }
+
+    public void RemoveText()
+    {
+        textDisplay.text = "";
+    }
+
+}
