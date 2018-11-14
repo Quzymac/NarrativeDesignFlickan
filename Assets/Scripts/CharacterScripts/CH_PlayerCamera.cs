674db311fa6a4e0cb129b80738445005f498a3cb
Claw03 <35917218+Claw03@users.noreply.github.com>
2018-11-14 16:19:23 +0100

    Camera script

---
+++ Assets/Scripts/CharacterScripts/CH_PlayerCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CH_PlayerCamera : MonoBehaviour {
+
+    [SerializeField]
+    private Transform camTransform;
+    [SerializeField]
+    private float sensitivity = 0.5f;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update () {
+            camTransform.Rotate(0, Input.GetAxis("Mouse X") * sensitivity, 0);
+	}
+}
