a382f0c138fcc74c5849d672ebe8cb20fd2e6e53
Claw03 <35917218+Claw03@users.noreply.github.com>
2018-11-14 12:01:31 +0100

    Update Assets/Scripts/PlayerMovement.cs

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody body;
+    [SerializeField]
+    private float moveSpeed;
+
+	void FixedUpdate () {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (direction.magnitude > 1)
+            Vector3.Normalize(direction);
+        direction *= moveSpeed;
+        direction.y = body.velocity.y;
+        body.velocity = direction;
+	}
+}
