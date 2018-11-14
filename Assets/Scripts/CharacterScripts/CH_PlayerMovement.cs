ed49ab35ffd4bf22c31166d777b9c548bcc37752
Quzymac <33623813+Quzymac@users.noreply.github.com>
2018-11-14 16:01:45 +0100

    Revert "Climbing and basic interact"
    This reverts commit dc0c025af9abc161c0778a7f4c69896a1325b0ed.

--- Assets/Scripts/CharacterScripts/CH_PlayerMovement.cs
+++ Assets/Scripts/CharacterScripts/CH_PlayerMovement.cs
@@ -9,71 +9,13 @@ public class CH_PlayerMovement : MonoBehaviour {
     [SerializeField]
     private float moveSpeed;
 
-    bool climbing;
-
-    private GameObject interactable;
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag.Equals("Climbable"))
-        {
-            climbing = true;
-        }
-        if (other.tag.Equals("Interactable"))
-        {
-            interactable = other.gameObject;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag.Equals("Climbable"))
-        {
-            climbing = false;
-        }
-        if (other.tag.Equals("Interactable"))
-        {
-            interactable.GetComponent<Interactable>().RemoveText();
-            interactable = null;
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag.Equals("Ground"))
-        {
-            climbing = false;
-        }
-    }
-
     void FixedUpdate()
     {
-        Vector3 direction;
-        
-
-        if (climbing)
-        {
-            //TODO Add climbing animation here.
-            direction = new Vector3(0f, Input.GetAxis("Vertical"), 0f);
-            if (direction.magnitude > 1)
-                Vector3.Normalize(direction);
-            direction *= moveSpeed;
-            direction.z = body.velocity.z;
-        }
-        else
-        {
-            //TODO Add running animation here.
-            direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            if (direction.magnitude > 1)
-                Vector3.Normalize(direction);
-            direction *= moveSpeed;
-            direction.y = body.velocity.y;
-        }
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (direction.magnitude > 1)
+            Vector3.Normalize(direction);
+        direction *= moveSpeed;
+        direction.y = body.velocity.y;
         body.velocity = direction;
-
-        if (Input.GetKeyDown("e") && interactable != null)
-        {
-            interactable.GetComponent<Interactable>().PlayText();
-        }
     }
 }
