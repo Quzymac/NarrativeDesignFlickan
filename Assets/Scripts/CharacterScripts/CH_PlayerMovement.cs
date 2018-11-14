96d95b3bf2f8b8008beef9f6eec7191f6e2c291e
Quzymac <33623813+Quzymac@users.noreply.github.com>
2018-11-14 13:37:39 +0100

    Terrain, PlayerPrefab, BushPrefab added

--- Assets/Scripts/CharacterScripts/CH_PlayerMovement.cs
+++ Assets/Scripts/CharacterScripts/CH_PlayerMovement.cs
@@ -2,19 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class CH_PlayerMovement : MonoBehaviour {
 
     [SerializeField]
     private Rigidbody body;
     [SerializeField]
     private float moveSpeed;
 
-	void FixedUpdate () {
+    void FixedUpdate()
+    {
         Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         if (direction.magnitude > 1)
             Vector3.Normalize(direction);
         direction *= moveSpeed;
         direction.y = body.velocity.y;
         body.velocity = direction;
-	}
+    }
 }
