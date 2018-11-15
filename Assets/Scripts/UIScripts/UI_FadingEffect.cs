6789e079f95c22cf8644468fc1d6f397a1c9eeed
TobenTheGreat <33621588+TobenTheGreat@users.noreply.github.com>
2018-11-15 15:59:22 +0100

    fade animations, music,

---
+++ Assets/Scripts/UIScripts/UI_FadingEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_FadingEffect : MonoBehaviour {
+
+    public GameObject Fade;
+
+	public void ActivateFading()
+    {
+        Fade.SetActive(true);
+    }
+
+    public void DeactivateFading()
+    {
+        Fade.SetActive(false);
+    }
+
+}
