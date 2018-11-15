7cd8150d3552186bb326e634ba0ff2d4c54e92c9
TobenTheGreat <33621588+TobenTheGreat@users.noreply.github.com>
2018-11-15 12:26:25 +0100

    Fungerande Menyer och Musik
    Har fixat Huvudmeny, Credits, quitgame och samt lagt till musik i spelet. Lagt till alla scenes i build manager. Credits Scenen gör för nuvarande ingenting.

---
+++ Assets/Scripts/UIScripts/LevelLoading.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoading : MonoBehaviour {
+
+    public static LevelLoading SceneSwitching;
+
+    private void Awake()
+    {
+        SceneSwitching = this;
+    }
+
+    public void SceneSwitch(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
