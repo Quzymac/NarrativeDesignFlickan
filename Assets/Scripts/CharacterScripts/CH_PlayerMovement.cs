f105a3c318fba203a9467e4d89ad7f15dc52df84
Claw03 <35917218+Claw03@users.noreply.github.com>
2018-11-19 14:04:31 +0100

    Movement, pushable och nya lager

--- Assets/Scripts/CharacterScripts/CH_PlayerMovement.cs
+++ Assets/Scripts/CharacterScripts/CH_PlayerMovement.cs
@@ -8,45 +8,31 @@ public class CH_PlayerMovement : MonoBehaviour
     [SerializeField]
     private Rigidbody body;
     [SerializeField]
-    private float moveSpeed = 3, rotationSpeed = 10;
+    private float defaultMoveSpeed = 3, defaultRotationSpeed = 10;
+    private float moveSpeed, rotationSpeed;
     [SerializeField]
     private Transform cameraTrans;
     private bool stop;
 
-    private GameObject interactable;
+    private void Start()
+    {
+        moveSpeed = defaultMoveSpeed;
+        rotationSpeed = defaultRotationSpeed;
+    }
 
     public void SetStop(bool b) //Starta/Stoppa spelarens normala movement (till exempel under dialog)
     {
         stop = b;
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        //If you walk in to an interactable 
-        if (other.tag.Equals("Interactable"))
-        {
-            interactable = other.gameObject;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        //If you walk out of an interactable 
-        if (other.tag.Equals("Interactable"))
-        {
-            interactable = null;
-        }
-    }
-
     void FixedUpdate()
     {
-
         if (!stop)
         {
-            Vector3 direction = Vector3.zero;
-
             /*** Vända sig ***/
             Vector3 inputDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));    //Input vector
+            if (inputDirection.magnitude > 1)
+                inputDirection = inputDirection.normalized;
             float cameraAngle = -Vector3.Angle(cameraTrans.forward, Vector3.forward);   //Kamerans vinkel jämfört med världen
             if (Vector3.Angle(cameraTrans.right, Vector3.forward) > 90f)                //Om kameran är åt vänster, 360 - vinkeln (Pga anledningar)
                 cameraAngle = 360 - cameraAngle;
@@ -54,22 +40,25 @@ public class CH_PlayerMovement : MonoBehaviour
             Vector3 newDirection = Vector3.RotateTowards(transform.forward, moveDirection, Time.deltaTime * rotationSpeed, 0);  //Den nya riktningen karaktären ska kolla mot
             transform.rotation = Quaternion.LookRotation(newDirection); //Rotera karaktären så den kollar mot den nya riktningen
 
-            /*** Gå ***/ //Ska ändra movement lite senare (gå segare medans man roterar typ)
-            float velocity = inputDirection.magnitude * moveSpeed; //Så att den typ accelererar lite + att den står stilla om man inte klickar något
-            if (velocity > 0)
-            {
-                //TODO Add running animation here. (Beroende på velocity så att den inte sprintar full speed instantly)
-                newDirection *= velocity; //Gångra med movespeed och ^ det där
-                newDirection.y = body.velocity.y;   //Vi vill inte ändra velocity i y led eftersom det skulle påvärka gravitationen
-                body.velocity = newDirection;   //GO! BLAH!
-            }
+            /*** Gå ***/
+            float speedForward = Vector3.Dot(transform.forward, moveDirection);
+            if (speedForward < 0)
+                speedForward = 0;
+            //TODO Add running animation here. (Beroende på velocity så att den inte sprintar full speed instantly)
+            Vector3 velocityVector = speedForward * moveDirection;
+            velocityVector *= moveSpeed; //Gångra med movespeed
+            velocityVector.y = body.velocity.y;   //Vi vill inte ändra velocity i y led eftersom det skulle påvärka gravitationen
+            body.velocity = velocityVector;   //GO! BLAH!
         }
-
-        //if you press e inside of an interactable
-        if (Input.GetKeyDown("e") && interactable != null)
+    }
+    public void SetSpeed(float moveSpeed, float rotationSpeed)
+    {
+        if (moveSpeed == 0)
         {
-            //Send the player gameobject to the interactable
-            interactable.GetComponent<Interactable>().Interact(gameObject);
+            moveSpeed = defaultMoveSpeed;
+            rotationSpeed = defaultRotationSpeed;
         }
+        this.moveSpeed = moveSpeed;
+        this.rotationSpeed = rotationSpeed;
     }
 }
