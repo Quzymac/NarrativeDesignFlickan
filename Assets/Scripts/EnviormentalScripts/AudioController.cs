22154a2193478225df1d3f5c0d0ed8361024d30a
TobenTheGreat <33621588+TobenTheGreat@users.noreply.github.com>
2018-11-20 16:03:50 +0100

    Musikområden, implementation av träd och andra objekt

---
+++ Assets/Scripts/EnviormentalScripts/AudioController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+    [SerializeField]
+    AudioSource gameMusic;
+    float thisMusic;
+    //bool hasExit = false;
+
+    private void OnTriggerExit(Collider other)
+    {
+        thisMusic = gameMusic.volume;
+        StartCoroutine(FadeOut(gameMusic, 4));
+        //hasExit = true;     
+    }
+  
+    private void OnTriggerEnter(Collider other)
+    {
+        /*if (hasExit == true)
+        {
+            StartCoroutine(FadeIn(gameMusic, 3));
+        }
+        hasExit = false;*/
+        StartCoroutine(FadeIn(gameMusic, 3));
+    }
+    public static IEnumerator FadeOut(AudioSource audioSource, float FadeOutTime)
+    {
+        float startVolume = audioSource.volume;
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeOutTime;
+            yield return null;
+        }
+
+        audioSource.Stop();
+        audioSource.volume = startVolume;
+    }
+
+    public static IEnumerator FadeIn(AudioSource audioSource, float FadeInTime)
+    {
+        float startVolume = audioSource.volume;      
+        audioSource.volume += startVolume * Time.deltaTime / FadeInTime;
+        yield return null;        
+        audioSource.Play();
+    }
+
+}
