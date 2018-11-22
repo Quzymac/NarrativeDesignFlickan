ec539497e020d7ea625be0072b53127e520afcea
Quzymac <33623813+Quzymac@users.noreply.github.com>
2018-11-22 16:19:18 +0100

    PostProcessing stuff, fairyEventScripts

---
+++ Assets/Scripts/StoryEventScript/FairyBarrier.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FairyBarrier : MonoBehaviour {
+
+    [SerializeField] Transform endPos; //Where the player will be pushed to
+    [SerializeField] GameObject barrier;//gameobject with collider to keep player out and particleEffect forceField
+    [SerializeField] GameObject player;
+
+    [SerializeField] float pushSpeed = 10f;
+   
+	void Update ()
+    {
+        //Only for testing 000000_____------000000_____------000000_____------000000_____------000000_____------000000_____------000000_____------000000_____------
+        //if (Input.GetKeyDown(KeyCode.P))
+        //{
+        //    StartCoroutine(PushPlayerAway());
+        //}
+    }
+
+    IEnumerator PushPlayerAway() //Call this when fairy is pushing player out of B3
+    {
+        float startTime = 0f;
+        player.GetComponent<CH_PlayerMovement>().SetStop(true);
+
+        while (true)
+        {
+            startTime += Time.deltaTime * (0.01f * pushSpeed);
+            player.transform.position = Vector3.Lerp(player.transform.position, endPos.transform.position, startTime);
+            player.transform.rotation = Quaternion.Lerp(player.transform.rotation, endPos.transform.rotation, startTime * 10);
+
+            if (Vector3.Distance(player.transform.position, endPos.transform.position) < 0.1f)    //player.transform.position == endPos.transform.position)
+            {
+                barrier.SetActive(true);
+                player.GetComponent<CH_PlayerMovement>().SetStop(false);
+
+                yield break;
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/StoryEventScript/FairyFoodCollecting.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FairyFoodCollecting : MonoBehaviour {
+
+    [SerializeField] GameObject player;
+    [SerializeField] Canvas countdownCanvas; //UI_Canvas
+    [SerializeField] Transform fairyGameOverPos;
+    [SerializeField] GameObject scriptManager;
+    [SerializeField] GameObject fadeInCanvas;
+    
+
+
+    void StartGame ()
+    {
+        countdownCanvas.GetComponent<CountdownTimer>().StartTimer();
+        FairyFollow(true);
+        Debug.Log("s");
+
+    }
+
+    public void GameTimerFinished()
+    {
+        FairyFollow(false);
+        StartCoroutine(TeleportToFairies());
+        Debug.Log("f");
+    }
+
+    IEnumerator TeleportToFairies()
+    {
+        //Fade WIP
+        scriptManager.GetComponent<UI_FadingEffect>().ActivateFading();
+        yield return new WaitForSeconds(1);
+        player.transform.position = fairyGameOverPos.position;
+        yield return new WaitForSeconds(0.8f);
+        fadeInCanvas.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+
+    }
+    void FairyFollow(bool following)
+    {
+        if (following)
+        {
+            //follow player
+        }
+        else
+        {
+            //stop following player
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P)){
+            StartGame();
+        }
+	}
+}
