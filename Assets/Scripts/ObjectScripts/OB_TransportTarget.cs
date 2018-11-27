cdd5a7f261541eba28999f0ee124e32dc88f7536
Quzymac <33623813+Quzymac@users.noreply.github.com>
2018-11-27 12:35:04 +0100

    OB_TransportTarget calculations methods finished

--- Assets/Scripts/ObjectScripts/OB_TransportTarget.cs
+++ Assets/Scripts/ObjectScripts/OB_TransportTarget.cs
@@ -6,134 +6,94 @@ using UnityEngine;
 public class OB_TransportTarget : MonoBehaviour {
 
     [SerializeField] List<GameObject> transportedItems;
-    GameObject transportedItem; //-----------
-
     [SerializeField] int itemsTier = 0;
+    [SerializeField] Item dialogItem;
+    int[] items = new int[10];
 
-    [SerializeField] Item mostOf;
+    public Item GetDialogItem()
+    {
+        Calculate();
+        return dialogItem;
+    }
+    public int GetItemsTier()
+    {
+        Calculate();
+        return itemsTier;
+    }
 
-    int[] items;
+    // Räknar ut vilket item det finns flest av och vilken tier det finns flest av
+    void Calculate()
+    {
+        
+        foreach (var item in transportedItems)
+        {
+            items[(int)item.GetComponent<OB_Item>().GetItemType()]++;
+        }
 
+        itemsTier = CalculateTier();
+        dialogItem = CalculateItems();
+    }
 
     int CalculateTier()
     {
-        return 1; //REMOVE--+000000+---REMOVE--+000000+---REMOVE--+000000+---REMOVE--+000000+---REMOVE--+000000+---REMOVE--+000000+---REMOVE--+000000+---
-
-        int numberOfItems = 0;
-
-        //Tier1
-        int apple = 0;
-        int blueBerry = 0;
-        int lingonberry = 0;
-        //Tier2
-        int chanterelle = 0;
-        int birchPolypore = 0;
-        int gulfotshatta = 0;
-
-        //Tier3
-        int flyAgaric = 0;
-        int pineCone = 0;
-        int firCone = 0;
-
-        items = new int[9];
-
-        foreach (var item in transportedItems)
+        if(items[(int)Item.Falukorv] > 0) // falukorv = tier 3
         {
-
-            items[(int)item.GetComponent<OB_Item>().GetItemType()]++;
-
-            numberOfItems++;
-
-            //switch (item.GetComponent<OB_Item>().GetItemType())
-            //{
-            //    case Item.Falukorv: // falukorv vinner alltid
-            //        mostOf = Item.Falukorv; 
-            //        return 3;
-            //    case Item.Apple:
-            //        apple++;
-            //        tierOne++;
-            //        break;
-            //    case Item.Blueberry:
-            //        blueBerry++;
-            //        tierOne++;
-            //        break;
-            //    case Item.Lingonberry:
-            //        lingonberry++;
-            //        tierOne++;
-            //        break;
-            //    case Item.Chanterelle:
-            //        chanterelle++;
-            //        tierTwo++;
-            //        break;
-            //    case Item.Birch_polypore:
-            //        birchPolypore++;
-            //        tierTwo++;
-            //        break;
-            //    case Item.Gulfotshatta:
-            //        gulfotshatta++;
-            //        tierTwo++;
-            //        break;
-            //    case Item.Fly_agaric:
-            //        flyAgaric++;
-            //        tierThree++;
-            //        break;
-            //    case Item.Pine_cone:
-            //        pineCone++;
-            //        tierThree++;
-            //        break;
-            //    case Item.Fir_cone:
-            //        firCone++;
-            //        tierThree++;
-            //        break;
-            //    default:
-            //        break;
-            //}
+            return 3;
         }
-
 
         int tierOne = items[(int)Item.Apple] + items[(int)Item.Blueberry] + items[(int)Item.Lingonberry];
         int tierTwo = items[(int)Item.Chanterelle] + items[(int)Item.Birch_polypore] + items[(int)Item.Gulfotshatta];
         int tierThree = items[(int)Item.Fir_cone] + items[(int)Item.Pine_cone] + items[(int)Item.Fly_agaric];
-
-        int random3 = (int)Random.Range(0f, 3f);
-        int random2 = (int)Random.Range(0f, 2f);
-
-
+        
         if (transportedItems.Count <= 3) //inte tillräckligt med items
         {
-            return 0;
+            return 3;
         }
-
-        Item[] tierOneItems = new Item[] { Item.Apple, Item.Blueberry, Item.Lingonberry };
-        Item[] tierTwoItems = new Item[] { Item.Chanterelle, Item.Birch_polypore, Item.Gulfotshatta };
-        Item[] tierThreeItems = new Item[] { Item.Fir_cone, Item.Pine_cone, Item.Fly_agaric };
-
         if (tierThree > tierTwo && tierThree > tierOne) // most of tier 3
         {
-            int maxValue = 0;
-            for (int i = 0; i < tierThreeItems.Length; i++)
-            {
-
-
-                //WIP
-
-                //if (myFruits[i] > maxValue)
-                //{
-                //    maxValue = myFruits[i];
-                //    maxIndex = i;
-            }
+            return 3;
         }
         else if (tierTwo > tierOne && tierTwo >= tierThree) //most of tier 2
         {
-            //tierTwo
+            return 2;
         }
         else // most of tier 1
         {
-            //tierOne
+            return 1;
         }
     }
 
+    Item CalculateItems() // räknar ut vilket item det finns flest av
+    {
+        if(items[(int)Item.Falukorv] > 0)
+        {
+            return Item.Falukorv;
+        }
 
+        int maxValue = 0;
+        int mostOfItem = 0;
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i] >= maxValue)
+            {
+                if (items[i] == maxValue) // random om det finns lika många
+                {
+                    int random = (int)Random.Range(0f, 100f);
+                    if (random % 2 == 0)
+                    {
+                        maxValue = items[i];
+                        mostOfItem = i;
+                    }
+                }
+                else
+                {
+                    maxValue = items[i];
+                    mostOfItem = i;
+                }
+            }
+        }
+        return (Item)mostOfItem;
+    }
 
     public bool SetObject(GameObject inObject)
     {
@@ -141,9 +101,7 @@ public class OB_TransportTarget : MonoBehaviour {
         {
             transportedItems.Add(inObject);
 
-            //--------
-            transportedItem = inObject;
-            Debug.Log("I have a " + transportedItem.name);
+            Debug.Log("I have a " + inObject.name);
             return true;
         }
         else
@@ -153,3 +111,51 @@ public class OB_TransportTarget : MonoBehaviour {
         }
     }
 }
+//Item[] tierOneItems = new Item[] { Item.Apple, Item.Blueberry, Item.Lingonberry };
+//Item[] tierTwoItems = new Item[] { Item.Chanterelle, Item.Birch_polypore, Item.Gulfotshatta };
+//Item[] tierThreeItems = new Item[] { Item.Fir_cone, Item.Pine_cone, Item.Fly_agaric };
+
+//switch (item.GetComponent<OB_Item>().GetItemType())
+//{
+//    case Item.Falukorv: // falukorv vinner alltid
+//        mostOf = Item.Falukorv; 
+//        return 3;
+//    case Item.Apple:
+//        apple++;
+//        tierOne++;
+//        break;
+//    case Item.Blueberry:
+//        blueBerry++;
+//        tierOne++;
+//        break;
+//    case Item.Lingonberry:
+//        lingonberry++;
+//        tierOne++;
+//        break;
+//    case Item.Chanterelle:
+//        chanterelle++;
+//        tierTwo++;
+//        break;
+//    case Item.Birch_polypore:
+//        birchPolypore++;
+//        tierTwo++;
+//        break;
+//    case Item.Gulfotshatta:
+//        gulfotshatta++;
+//        tierTwo++;
+//        break;
+//    case Item.Fly_agaric:
+//        flyAgaric++;
+//        tierThree++;
+//        break;
+//    case Item.Pine_cone:
+//        pineCone++;
+//        tierThree++;
+//        break;
+//    case Item.Fir_cone:
+//        firCone++;
+//        tierThree++;
+//        break;
+//    default:
+//        break;
+//}
