d18779894bf44939bd3284c2e88ec123d08cdc35
TobenTheGreat <33621588+TobenTheGreat@users.noreply.github.com>
2018-11-28 12:11:35 +0100

    Pushar B3 och sista version inför testing

---
+++ Assets/Scripts/EnviormentalScripts/BirdSounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class BirdSounds : MonoBehaviour {
+
+    [SerializeField]
+    AudioClip[] Bird = new AudioClip[9];
+    AudioSource birdSounds;
+
+    private void Start()
+    {
+        birdSounds = gameObject.GetComponent<AudioSource>();
+        birdSounds.Play();
+    }
+}
