bb4df9a1688bd46718b48f0cea240df1beb4849d
LordCommanderAlokin <niki_96_obrez@hotmail.com>
2018-11-29 16:02:43 +0100

    Materials för ljuset och script för teleport

---
+++ Assets/Scripts/CharacterScripts/CH_TeleportPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CH_TeleportPlayer : MonoBehaviour {
+
+    [SerializeField] Transform position1;
+    //[SerializeField] Transform position2;
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if(c.gameObject.tag == "Player")
+        {
+            c.gameObject.transform.position = position1.position;
+        }
+    }
+}
