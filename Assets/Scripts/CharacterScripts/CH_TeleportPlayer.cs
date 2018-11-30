af9ce825ac9f53d6b60e02f3d2f2c588af27cbf6
LordCommanderAlokin <niki_96_obrez@hotmail.com>
2018-11-30 15:44:51 +0100

    House

--- Assets/Scripts/CharacterScripts/CH_TeleportPlayer.cs
+++ Assets/Scripts/CharacterScripts/CH_TeleportPlayer.cs
@@ -2,16 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CH_TeleportPlayer : MonoBehaviour {
+public class CH_TeleportPlayer : OB_Interactable {
 
     [SerializeField] Transform position1;
-    //[SerializeField] Transform position2;
 
-    private void OnTriggerEnter(Collider c)
+    UI_FadingEffect fadeCanvas;
+
+    private void Start()
     {
-        if(c.gameObject.tag == "Player")
-        {
-            c.gameObject.transform.position = position1.position;
-        }
+        fadeCanvas = FindObjectOfType<UI_FadingEffect>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        OnEnter(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OnExit(other);
+    }
+
+    public override void Activate(GameObject player)
+    {
+        StartCoroutine(Bla(player));
+        
+
+    }
+
+    IEnumerator Bla(GameObject player)
+    {
+        fadeCanvas.ActivateFading();
+        yield return new WaitForSeconds(1);
+        player.transform.position = position1.position;
+
+        fadeCanvas.DeactivateFading();
     }
 }
