9288505ba54219c386af9468aacd80f7d27bd119
TobenTheGreat <33621588+TobenTheGreat@users.noreply.github.com>
2018-11-30 13:02:45 +0100

    Nya props

---
+++ Assets/Scripts/CharacterScripts/CH_MammaTroll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CH_MammaTroll : MonoBehaviour {
+
+    [SerializeField]
+    Transform mammaGoal;
+
+    NavMeshAgent troll;
+
+    [SerializeField]
+    GameObject mammaTroll;
+
+    void Start()
+    {
+        troll = GetComponent<NavMeshAgent>();
+        troll.destination = mammaGoal.position;      
+    }
+    private void Update()
+    {
+        if (troll.transform.position == mammaGoal.transform.position)
+        {
+            Destroy(mammaTroll);
+        }
+    }
+}
