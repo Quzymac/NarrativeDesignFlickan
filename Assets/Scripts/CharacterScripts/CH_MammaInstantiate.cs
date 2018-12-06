b013afef7e0e876907b97bcda29ebe0690178a23
Flippeen <filip.k.lindgren@gmail.com>
2018-12-06 11:36:46 +0100

    Mamma Ny

--- Assets/Scripts/CharacterScripts/CH_MammaInstantiate.cs
+++ Assets/Scripts/CharacterScripts/CH_MammaInstantiate.cs
@@ -26,7 +26,7 @@ public class CH_MammaInstantiate : MonoBehaviour {
             mamma = Instantiate(MammaTroll, spawnposition, Quaternion.identity).GetComponent<CH_MammaTroll>();          
             mamma.SetMammaGoal(GameObject.FindGameObjectWithTag("MammaGoal").transform);
             firstSpawn = true;
-            UI_DialogueController.Instance.DisplayMessage("Tyra", "Trollmamman tog min lillebror! Jag måste få tillbaka honom från trollet! jag borde följa efter för att se vart hon kan leda mig", 8f);
+            UI_DialogueController.Instance.DisplayMessage("Tyra", "Är det där ett troll som springer iväg med Hilding?! Jag måste hinna ikapp!", 8f);
         }
         else
         {
