82ed6d95e6b7ffcc5722b8b0f8cf44d3c203e8cd
TobenTheGreat <33621588+TobenTheGreat@users.noreply.github.com>
2018-12-10 13:59:08 +0100

    Nya dörrar och dark souls easter egg

---
+++ Assets/Scripts/EnviormentalScripts/RoadB2Interact.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadB2Interact : OB_Interactable {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnEnter(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OnExit(other);
+    }
+
+    public override void Activate(GameObject player)
+    {
+        UI_DialogueController.Instance.DisplayMessage("Tyra", "Snok har jag sett förut men aldrig skulle jag fått för mej att det fanns ormar så stora uti Ärlaskogen.", 6);
+            
+    }
+
+}
